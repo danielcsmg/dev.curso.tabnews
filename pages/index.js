30cfa307ce59a7c32eb96df262a488e29b959088
Daniel Caitano <daniel.csouzamg@gmail.com>
2026-03-01 15:06:42 +0000

    Atualiza página `index.js`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home() {
-    return <h1>Mirian, meu amor, se você me ama, dá uma risadinha!!!</h1>
+    return <div>
+            <h1>Mirian, meu amor, se você me ama, dá uma risadinha!!!</h1>
+            <h2>Você é a mulher mais linda desse mundo!</h2>
+        </div>
 }
 
 export default Home;
